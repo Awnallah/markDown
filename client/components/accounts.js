346cb51b0924b0c7f27d51a2e5d53884a0859c33
Hamzah <6hamzah6@gmail.com>
2016-09-03 23:20:17 -0700

    added accounts component

---
+++ client/components/accounts.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Accounts extends Component {
+	componentDidMount() {
+		// Render Blaze accounts form then place it in the div within the render method
+
+	}
+
+	componentWillUnmount() {
+		// find created forms and destroy them
+	}
+	render() {
+		return (
+			<div></div>
+			);
+	}
+}
+
+
+export default Accounts;
