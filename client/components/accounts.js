0ced493a16383fbd821409b3a051dababaf49427
Hamzah <6hamzah6@gmail.com>
2016-09-04 16:36:10 -0700

    added blaze authentication

--- client/components/accounts.js
+++ client/components/accounts.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import { Template } from 'meteor/templating'
+import { Blaze } from 'meteor/blaze';
+
 
 class Accounts extends Component {
 	componentDidMount() {
 		// Render Blaze accounts form then place it in the div within the render method
-
+		this.view = Blaze.render(Template.loginButtons,
+		 ReactDom.findDOMNode(this.refs.container));
 	}
 
 	componentWillUnmount() {
 		// find created forms and destroy them
+		Blaze.remove(this.view);
 	}
 	render() {
 		return (
-			<div></div>
+			<div ref='container'></div>
 			);
 	}
 }
