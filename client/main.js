d3d8e50bda112e47117199e59662a30d8d4ececb
Hamzah <6hamzah6@gmail.com>
2016-09-11 15:37:20 -0700

    BinsMain component and routing added

--- client/main.js
+++ client/main.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './components/app';
+import BinsMain from './components/bins/bins_main';
+import BinsList from './components/bins/bins_list';
 import { Bins } from '../imports/collections/bins';
 
+//routes: contains all mapping btw urls and components
+const routes = (
+	<Router history={browserHistory}>
+		<Route path='/' component={App}>
+			<Route path='bins' component={BinsMain} />
+		</Route>
+	</Router>
+
+	);
+
 Meteor.startup(() => {
-	ReactDom.render(<App />, document.querySelector('.render-target'))
+	ReactDom.render(routes, document.querySelector('.render-target'))
 });
