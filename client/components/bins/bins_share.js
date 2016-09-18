d316c1fcbb466c90e1bba441e18e58e2048d9f4c
Hamzah <6hamzah6@gmail.com>
2016-09-18 16:01:22 -0700

    bins.share method added

--- client/components/bins/bins_share.js
+++ client/components/bins/bins_share.js
@@ -5,7 +5,14 @@ class BinsShare extends Component {
 	render() {
 		return (
 			<footer className='bins-share'>
-				BinShare
+				<div className='input-group'>
+					<input className='form-control' />
+					<div className='input-group-btn'>
+						<button  className="btn btn-default">
+							Share Bin
+						</button>
+					</div>
+				</div>
 			</footer>
 			);
 	}
