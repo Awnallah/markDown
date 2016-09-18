502347f21c896ef884a561d34e9a9eb48e283678
Hamzah <6hamzah6@gmail.com>
2016-09-18 12:00:40 -0700

    for viewing the markdown

---
+++ client/components/bins/bins_viewer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class BinViewer extends Component {
+	render() {
+		return (
+			<div className='col-xs-4'>
+			<h5>Output</h5>
+			</div>
+			);
+	}
+
+}
+
+export default BinViewer;
