4d7a367b83a7e35281f5ffcc239326035548a48b
Hamzah <6hamzah6@gmail.com>
2016-09-18 16:44:41 -0700

    display shared with emails

--- client/components/bins/bins_share.js
+++ client/components/bins/bins_share.js
@@ -2,16 +2,36 @@ import React , { Component } from 'react';
 
 
 class BinsShare extends Component {
+	onShareClcik() {
+		const email = this.refs.email.value;
+		Meteor.call('bins.share', this.props.bin, email);
+
+	}
+
+	renderShareList() {
+		return this.props.bin.sharedWith.map(email => {
+			return <button key={email}  className="btn btn-default">{email}</button>
+		});
+	}
+
 	render() {
 		return (
 			<footer className='bins-share'>
 				<div className='input-group'>
-					<input className='form-control' />
+					<input ref='email' className='form-control' />
 					<div className='input-group-btn'>
-						<button  className="btn btn-default">
+						<button
+							 onClick={this.onShareClcik.bind(this)}
+							 className="btn btn-default">
 							Share Bin
 						</button>
 					</div>
+				</div>
+				<div>
+					Shared With:
+				</div>
+				<div className='btn-group'>
+					{this.renderShareList()}
 				</div>
 			</footer>
 			);
