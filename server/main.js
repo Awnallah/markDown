725290271a5b24f7d01cb9139b8f2a2c61934a2a
Hamzah <6hamzah6@gmail.com>
2016-09-19 19:05:16 -0700

    sharedBins publication method added

--- server/main.js
+++ server/main.js
@@ -3,8 +3,21 @@ import { Bins } from '../imports/collections/bins';
 
 Meteor.startup(() => {
   Meteor.publish('bins', function() {
-
   	return Bins.find({ ownerId: this.userId });
 
+  });
+
+
+  //shared bins
+  Meteor.publish('sharedBins', function() {
+  	const user = Meteor.users.findOne(this.userId);
+
+  	if (!user) { return; } //when user isn't logged in
+
+  	const email = user.emails[0].address;
+
+  	return Bins.find({
+  		shardWith: { $elemMatch: { $eq: email } }
+  	});
   });
 });
